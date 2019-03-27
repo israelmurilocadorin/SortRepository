f64049c9437716e0ed4f6b0a9398cc20a113ecf5
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2019-03-27 10:16:19 -0300

    Método finalizado e arruamando indentação

--- InsertionSort.php
+++ InsertionSort.php
@@ -1,36 +1,42 @@
+<?php
 class InsertionSort{
-  //atributos
-  public $i;
-  public $j;
-  public $aux;
-  public $vetor = array();
+    //atributos
+    public $i;
+    public $j;
+    public $aux;
+    public $vetor = array();
 
-  function __construct() {
-    for($z=0; $z<5; $z++){
-      $this->vetor[$z] = rand(0, 100);  	 
-    }
-  }
-
-  public function printVetor() {
-    return $this->vetor;
-  }
-
-  public function ordena(){
-    for($i=1; $i < sizeof($this->vetor); $i++) {
-      $aux = $this->vetor[$i];
-      $j = $i - 1;
-      while (($j >= 0) && ($aux < $this->vetor[$j]))
-      {
-        $this->vetor[$j + 1] = $this->vetor[$j];
-        $j--;
+    function __construct() {
+      for($z=0; $z<5; $z++){
+        $this->vetor[$z] = rand(0, 100);  	 
       }
-      $this->vetor[$j+1] = $aux;
     }
-  }      
+
+    public function printVetor() {
+      return $this->vetor;
+    }
+
+    public function ordena(){
+
+      for($i=1; $i < sizeof($this->vetor); $i++) {
+        $aux = $this->vetor[$i];
+        $j = $i - 1;
+
+        while (($j >= 0) && ($aux < $this->vetor[$j]))
+        {
+          $this->vetor[$j + 1] = $this->vetor[$j];
+          $j--;
+        }
+        
+        $this->vetor[$j+1] = $aux;
+      }
+
+    }      
 }
-  		
-  		//--main--//
-  		$a = new InsertionSort();
-  		var_dump($a->printVetor());
-  		$a->ordena();
-  		print_r($a->printVetor());
+    
+    //--main--//
+    $a = new InsertionSort();
+    var_dump($a->printVetor());
+    $a->ordena();
+    print_r($a->printVetor());
+?>
