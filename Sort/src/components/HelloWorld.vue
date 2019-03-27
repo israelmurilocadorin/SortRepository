8bdcd63387dfb3d44c1a12338e442a6e9e894e17
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2019-03-27 01:06:47 -0300

    projeto em vue

--- Sort/src/components/HelloWorld.vue
+++ Sort/src/components/HelloWorld.vue
@@ -1,113 +1,79 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="container">
+      <h1>{{ msg }}</h1>
+
+      <div class="row"> 
+        <div class="col-md-12">
+          <div class="help">?
+            <span class="msg-help">das</span>
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <div class="input-group input-group-lg">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-lg">Lista de números:</span>
+            </div>
+            <input type="text" v-model="array.valor" :placeholder="texto_place_holder" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm">
+          </div> 
+        </div>
+      </div>
+     
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <button @click="limparTexto" class="btn btn-outline-secondary" type="button">Limpar</button>
+        </div>        
+        <div class="col-md-6">
+          <button @click="ordenar" class="btn btn-outline-secondary" type="button">Ordenar</button>
+        </div>
+      </div>
+
+    </div><!--/container-->
   </div>
 </template>
-
 <script>
+
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Ordenação de Lista',
+      texto_place_holder: 'Digite aqui ...',
+      array: {
+        type: String,
+        valor: ""
+      }
+    }
+  },
+
+  methods: {
+    ordenar: function(){
+      console.log("ARRAY PARA SER ORDENADO:")
+      console.log(this.array.valor.split(" "))
+    },
+    limparTexto: function(){
+      this.array.valor = ''
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+button{
+  width:100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.help{
+  display:inline-block;
+  float:right;
+  font-weight: bold;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.help:hover{
+
 }
-a {
-  color: #42b983;
+.msg-help{
+  display:none;
 }
 </style>
--- Sort/src/router/index.js
+++ Sort/src/router/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vuetify from 'vuetify'
 import HelloWorld from '@/components/HelloWorld'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+Vue.use(BootstrapVue)
 Vue.use(Router)
 
 export default new Router({
