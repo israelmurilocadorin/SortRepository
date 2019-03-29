43c6e0c4a7101e99924cbe99c92923d9771bbe8e
dayanOrlando <dayan@gatilabs.com.br>
2019-03-29 17:32:54 -0300

    ShellSort identado e tag php add

--- ShellSort.php
+++ ShellSort.php
@@ -1,37 +1,39 @@
-class shellSort {
-
-     function __construct() {
-        for($z = 0; $z < 5; $z++) {
-            $this->vetor[$z] = rand(0, 100);  	 
-        }
-    }
-    
-    public function printVetor() {
-        return $this->vetor;
-    }
-
-    function ordenar() {
-
-        $x = round(count($this->vetor) / 2);
-
-        while($x > 0) {
-            for($i = $x; $i < count($this->vetor); $i++) {
-                $temp = $this->vetor[$i];
-                $j = $i;
-                while($j >= $x && $this->vetor[$j-$x] > $temp) {
-                    $this->vetor[$j] = $this->vetor[$j - $x];
-                    $j -= $x;
-                }
-                $this->vetor[$j] = $temp;
+<?php
+    class shellSort {
+        
+        function __construct() {
+            for($z = 0; $z < 5; $z++) {
+                $this->vetor[$z] = rand(0, 100);  	 
             }
-            $x = round($x / 2.2);
         }
-        return $this->vetor;
+        
+        public function printVetor() {
+            return $this->vetor;
+        }
+
+        function ordenar() {
+            
+            $x = round(count($this->vetor) / 2);
+
+            while($x > 0) {
+                for($i = $x; $i < count($this->vetor); $i++) {
+                    $temp = $this->vetor[$i];
+                    $j = $i;
+                    while($j >= $x && $this->vetor[$j-$x] > $temp) {
+                        $this->vetor[$j] = $this->vetor[$j - $x];
+                        $j -= $x;
+                    }
+                    $this->vetor[$j] = $temp;
+                }
+                $x = round($x / 2.2);
+            }
+            return $this->vetor;
+        }
     }
-}
 
-$a = new shellSort();
-var_dump($a->printVetor());
+    $a = new shellSort();
+    print_r($a->printVetor());
 
-$a->ordenar();
-print_r($a->printVetor());
+    $a->ordenar();
+    print_r($a->printVetor());
+?>
